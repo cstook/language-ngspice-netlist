d85f8ac438470db9135015897c35002412b13b91
Chris Stook <chris@chrisstook.com>
2018-02-25 12:30:27 -0500

    initial commit

---
+++ spec/language-ngspice-netlist-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageNgspiceNetlistView from '../lib/language-ngspice-netlist-view';
+
+describe('LanguageNgspiceNetlistView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
